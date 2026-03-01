3c738bc91e6db6a88885546b138716e48f84a761
kubota-a <kubota24.ak@gmail.com>
2026-03-01 22:55:29 +0900

    feat: 初期マイグレーションを作成し、NeonDB に反映

--- mvp-01-role-todo/models.py
+++ mvp-01-role-todo/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy  # SQLAlchemy（ORM：Pythonのクラス
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import CheckConstraint, text
-# from sqlalchemy.sql import func  # Postgresでも安全な日時関数
+from sqlalchemy.sql import func  # Postgresでも安全な日時関数
 
 
 # アプリ全体で共有する DB オブジェクト
